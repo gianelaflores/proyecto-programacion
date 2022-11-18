9cdb7085c9aa943c0a6093036c98b22ccbcf4fb4
gianelaflores <111946654+gianelaflores@users.noreply.github.com>
2022-11-18 08:59:27 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -3,3 +3,7 @@ import pandas as pd
 import numpy as np
 
 st.title('SUNEDU')
+st.subheader('Instituciones licenciadas')
+url=https://github.com/gianelaflores/proyecto-programacion/blob/documentos/Licenciamiento%20Institucional_7.csv1
+file=pd.read_csv(url, sep=',')
+st.line_chart(data=file,x='Nombre de universidad', y='Periodo de licenciamiento')
