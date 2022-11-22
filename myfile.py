ef5c2e6beff37843b13e8df0f893d530b53aa595
gianelaflores <111946654+gianelaflores@users.noreply.github.com>
2022-11-21 20:15:59 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from datatime import time
+from datetime import time
 appointment = st.slider("Programe la asesoria:",value=(time(11, 30), time(12, 45)))
 st.write("Esta agendado para:", appointment)
 
