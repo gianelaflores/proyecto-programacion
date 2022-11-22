5fd9e76de7bfa4ee3a30f3310687e238a5fea487
gianelaflores <111946654+gianelaflores@users.noreply.github.com>
2022-11-21 20:10:24 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from datatime import time
+appointment = st.slider("Programe la asesoria:",value=(time(11, 30), time(12, 45)))
+st.write("Esta agendado para:", appointment)
+
 
 st.title('SUNEDU')
 st.subheader('Instituciones licenciadas')
@@ -8,5 +12,4 @@ url='https://raw.githubusercontent.com/gianelaflores/proyecto-programacion/docum
 file=pd.read_csv(url, sep=',')
 st.line_chart(data=file,x='CODIGO_ENTIDAD', y='PERIODO_LICENCIAMIENTO')
 st.subheader('Tabla de datos')
-
 st.table(data=file,columns=('CODIGO_ENTIDAD','NOMBRE'))
