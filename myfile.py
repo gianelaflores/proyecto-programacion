927c7f450f37c504a7a905ce439de514d86f095a
Gandy2704 <117965553+Gandy2704@users.noreply.github.com>
2022-11-21 19:44:32 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -7,3 +7,6 @@ st.subheader('Instituciones licenciadas')
 url='https://raw.githubusercontent.com/gianelaflores/proyecto-programacion/documentos/Licenciamiento%20Institucional_7.csv'
 file=pd.read_csv(url, sep=',')
 st.line_chart(data=file,x='CODIGO_ENTIDAD', y='PERIODO_LICENCIAMIENTO')
+st.subheader('Tabla de datos')
+
+st.table(data=file, 'CODIGO_ENTIDAD', 'NOMBRE')
