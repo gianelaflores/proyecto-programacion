40c508d72be7d91f58092a9a16d223d80ff38819
gianelaflores <111946654+gianelaflores@users.noreply.github.com>
2022-12-02 15:54:19 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -1,15 +1,16 @@
+# pip install streamlit --upgrade
 import streamlit as st
 import pandas as pd
 import numpy as np
 from streamlit_option_menu import option_menu
 
 
-st.set_page_config(layout="wide",initial_sidebar_state='expanded')
-with open('upch.css') as f :
-    st.markdown(f'<style>{f.read()}</upch>',unsafe_allow_html=True)
+st.set_page_config(layout="wide", initial_sidebar_state='expanded')
+with open('logo.css) as f : 
+    st.markdown(f'<style>{f.read()}</logo>',unsafe_allow_html=True)
 with st.sidebar:
     st.markdown('###')
-    st.sidebar.header ('Programación')
+    st.sidebar.header ('Programación') 
     selected= option_menu(
         menu_title='Menú',
         options=['Inicio','Equipo','Analisis','reporte'],
