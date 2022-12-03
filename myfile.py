e9712701adedfa008a4dc75992a8dfdad63674c6
gianelaflores <111946654+gianelaflores@users.noreply.github.com>
2022-12-02 21:11:35 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -30,8 +30,7 @@ if selected == 'Inicio':
     st.markdown("<h1 style ='text-align: center'>LICENCIAMIENTO INSTITUCIONAL</h1>", unsafe_allow_html=True)
     st.markdown("---")
     st.subhead("¿Que es SUNEDU?")
-    st.write("La Superintendencia Nacional de Educación Superior Universitaria (SUNEDU) es el organismo público adscrito al Ministerio de Educación, que garantiza una oferta educativa de calidad en favor de los estudiantes, a través del licenciamiento y supervisión, con eficiencia, predictibilidad, transparencia y respeto a la autonomía universitaria.
-")
+    st.write("La Superintendencia Nacional de Educación Superior Universitaria (SUNEDU) es el organismo público adscrito al Ministerio de Educación, que garantiza una oferta educativa de calidad en favor de los estudiantes, a través del licenciamiento y supervisión, con eficiencia, predictibilidad, transparencia y respeto a la autonomía universitaria.")
     st.header("Dataset")
     @st.experimental_memo
              
